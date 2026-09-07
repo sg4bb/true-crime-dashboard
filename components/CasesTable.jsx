222d2c0d6dec29a65a400a10fc505c412d042de2
Gabriel <gaabrielserraa@gmail.com>
2026-09-07 01:57:56 -0400

    Last change on the 'Agency' column with empty values

--- components/CasesTable.jsx
+++ components/CasesTable.jsx
@@ -429,8 +429,8 @@ export default function CasesTable() {
                   </div>
                 </td>
                 <td className="px-3 py-2">
-                  <span className="flex w-full justify-center px-1 py-1 rounded text-xs border border-neutral-700 text-neutral-400 truncate">
-                    {c.pd}
+                  <span className="flex w-full px-1 py-1 rounded text-xs border border-neutral-700 text-neutral-400 truncate">
+                    {c.pd || "ㅤ"}
                   </span>
                 </td>
                 <td className="px-3 py-2 text-neutral-200 truncate">{c.suspect}</td>
